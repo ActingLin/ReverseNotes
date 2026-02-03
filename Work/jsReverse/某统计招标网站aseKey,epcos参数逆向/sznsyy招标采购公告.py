733733bb7bd76981ef10953b19da519630530fd1
ActingLin <act_lin307169@outlook.com>
2026-02-03 22:48:30 +0800

    某统计招标网站aseKey,epcos参数逆向

---
+++ Work/jsReverse/某统计招标网站aseKey,epcos参数逆向/sznsyy招标采购公告.py
@@ -0,0 +1,14 @@
+# -*- coding: utf-8 -*-
+"""
+@File    : sznsyy招标采购公告.py
+@Author  : Elliot Lin
+@Date    : 2026/2/3 22:36
+@Project : ReverseNotes
+@Github  : https://github.com/ActingLin/ReverseNotes
+@Desc    : 采集招标公告PDF文件
+"""
+
+
+url = 'https://zbcg.sznsyy.cn/homeNotice'
+# https://zbcg.sznsyy.cn/sz/file/download?epcos=a2dMcCtDWXd3WnpIanYxcDgyZC9hSGQzbWFmUWxreHRCeDNuRVZXcUpPWm52UTNZTnZpZXYrUXZ3MFg1U1ZFcg
+# PDF下载
